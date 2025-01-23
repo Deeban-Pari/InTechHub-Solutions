e7325d13ccf803510230b9d69839ad5a81aeeca9
Deeban-Pari <deebanpari2001@gmail.com>
2025-01-23 15:53:09 +0530

    first commit

---
+++ InTechHub/Models/ProductContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace InTechHub.Models
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
